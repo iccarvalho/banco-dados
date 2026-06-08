c2d1330604f866951d295989ab436f304d778eef
iccarvalho <igccarvalho06@outlook.com>
2026-06-08 17:56:22 -0300

    ex004

---
+++ exercicios/ex004.sql
@@ -0,0 +1,36 @@
+USE Hospital;
+
+SELECT * FROM medicos;
+SELECT * FROM especialidades;
+SELECT * FROM consultas;
+SELECT * FROM pacienets;
+
+-- 1) Cadastre 6 pacientes
+
+-- 2) Cadastre 10 consultas para médicos e pacientes diversos
+
+-- 3) Atualize o nome do Médico 'JOÃO' para 'JOÃO DA SILVA'
+
+-- 4) A data da consulta número 3 é 15/maio/2026 (atualize essa informação)
+
+-- 5) Exclua a primeira consulta cadastrada
+
+-- 6) Liste os nomes dos médicos e a especialidade de cada um.
+
+-- 7) Liste os médicos que não têm especialidades.
+
+-- 8) Liste as consultas feitas pelo convêncio UNIMED no mês de abril.
+
+-- 9) Liste os nomes dos pacientes e os convênios que usaram nas suas consultas.
+
+-- 10) Liste os telefones dos pacientes que nunca consultaram.
+
+-- 11) Liste os convênios das consultas feitas por ORTOPEDISTAS.
+
+-- 12) Liste os nomes e fones dos pacientes atendidos por PEDIATRAS ou DERMATOLOGISTAS em abril/2026.
+
+-- 13) Cadastre a especialidade 'NEUROLOGISTA' e atualize as especialidades de 2 médicos para esta nova.
+
+-- 14) Crie 3 consultas para médicos NEUROLOGISTAS no mês de maio/2026.
+
+-- 15) As consultas feitas pelos PEDIATRAS em abril/2026 devem se somente do convênio 'SUS'. Atualize essa informação.
